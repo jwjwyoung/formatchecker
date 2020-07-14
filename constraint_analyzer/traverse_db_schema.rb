559133d5c288ff64354f94828ea7db1be9be047b
Wang Ruochen <wangkevin920@163.com>
2020-07-14 20:50:47 +0800

    Add counter for add/del

--- constraint_analyzer/traverse_db_schema.rb
+++ constraint_analyzer/traverse_db_schema.rb
@@ -1,26 +1,32 @@
 class Version_class
   def compare_db_schema(old_vers)
+    col_add = col_del = tab_add = tab_del = 0
     @activerecord_files.each_key do |key|
       file = @activerecord_files[key]
       old_file = old_vers.activerecord_files[key]
       unless old_file
         # missing old file: added table
         puts "add table #{key}: #{file.filename}@#{@commit[..8]}"
+        tab_add += 1
         next
       end
       # present in old file but missing in new file: deleted column
       old_file.columns.each_key.reject { |k| file.columns.keys.include? k }.each do |col|
         puts "del column #{col} in table #{key}: #{file.filename}@#{@commit[..8]}"
+        col_del += 1
       end
       # present in new file but missing in old file: added column
       file.columns.each_key.reject { |k| old_file.columns.keys.include? k }.each do |col|
         puts "new column #{col} in table #{key}: #{file.filename}@#{@commit[..8]}"
+        col_add += 1
       end
     end
     old_vers.activerecord_files.each_key.reject { |k| @activerecord_files[k] }.each do |key|
       # missing new file: delete table
       puts "del table #{key}: #{@commit[..8]}"
+      tab_del += 1
     end
+    [col_add, col_del, tab_add, tab_del]
   end
 end
 
@@ -47,6 +53,7 @@ def traverse_all_for_db_schema(app_dir, interval = nil)
   versions.map! { |v| build_version(version_his_folder, v) }
   # newest versions comes first
   versions.each_cons(2).each do |newv, curv|
-    newv.compare_db_schema(curv)
+    col_add, col_del, tab_add, tab_del = newv.compare_db_schema(curv)
+    puts "#{newv.commit[..7]} #{col_add}, #{col_del}, #{tab_add}, #{tab_del}"
   end
 end
