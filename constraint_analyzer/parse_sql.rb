a426a4bcb1f88e50fe0e6b106496b5e6095deeab
Wang Ruochen <wangkevin920@163.com>
2020-07-25 16:51:46 +0800

    Deal with SQL parse errors
    - fix quote escape
    - catch exception and ignore it

--- constraint_analyzer/parse_sql.rb
+++ constraint_analyzer/parse_sql.rb
@@ -23,8 +23,15 @@ end
 
 # use pg query to handle sql query string
 def parse_sql_string(sql)
+  # in mastodon, we have:
+  # "CREATE INDEX hashtag_search_index ON tags USING gin(to_tsvector(\\'simple\\', tags.name));"
+  sql.gsub!("\\'", "'")
   type = ""
-  sql_ast = PgQuery.parse(sql)
+  begin
+    sql_ast = PgQuery.parse(sql)
+  rescue PgQuery::ParseError
+    return
+  end
   tree = sql_ast.tree[0]["RawStmt"]["stmt"]
   table_name = nil
   columns = []
